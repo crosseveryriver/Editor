884be79c5dcd083ea64805c3228aa6abccfb688c
crosseveryriver <1304138563@qq.com>
2017-03-04 18:53:22 +0800

    Many Htmls

---
+++ src/main/webapp/backend/ckeditor/config.js
@@ -0,0 +1,7 @@
+CKEDITOR.editorConfig = function( config ) {
+    config.language = 'zh-cn';
+    config.uiColor = '#F8F8F8';
+    config.extraPlugins = 'autogrow';
+    config.autoGrow_minHeight = 250;
+    config.autoGrow_maxHeight = 600;
+};
