98a1230e9a954077f8cd5281fa11cf4df9594400
cristaessica <544825765@qq.com>
2017-03-07 00:11:43 +0800

    保存文章

---
+++ src/main/webapp/js/article.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Erika on 2017/3/6.
+ */
+window.addEventListener("load", function(){
+    handleSave();
+});
+
+function handleSave(){
+    $("#save-btn").on("click",function(){
+        $.ajax({
+            url:"/articles/save",
+            type:"post",
+            data:{
+                title:$("#head").val(),
+                html:CKEDITOR.instances.editor1.getData()
+            },
+            success:function(data) {
+                // 成功以后要做的事
+                alert("safds");
+            },
+            error:function(jqXhr,textStatus,errorThrown){
+                console.log(errorThown);
+            }
+
+        });
+    });
+}
