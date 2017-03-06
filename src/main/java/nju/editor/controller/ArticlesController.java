09d6e8c0607c56f842b1d16bb6c38cacd66973bd
wangzehao <wzhkun@163.com>
2017-03-07 04:55:46 +0800

    article.html里报错，不知道为什么，改了一下还是报错

--- src/main/java/nju/editor/controller/ArticlesController.java
+++ src/main/java/nju/editor/controller/ArticlesController.java
@@ -1,11 +1,14 @@
 package nju.editor.controller;
 
+import nju.editor.model.Article;
 import nju.editor.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * Created by wangzehao on 2017/3/7.
@@ -18,13 +21,15 @@ public class ArticlesController {
 
     @RequestMapping("/article")
     public String article(@RequestParam(value="id", required=false, defaultValue = "null") String id, Model model){
-        model.addAttribute(articleService.getReleasedArticleById(Long.parseLong(id)));
+        Article article=articleService.getReleasedArticleById(Long.parseLong(id));
+        model.addAttribute(article);
         return "article";
     }
 
     @RequestMapping("/articles")
     public String articles(Model model){
-        model.addAttribute(articleService.allEditing());
+        List<Article> articleList=articleService.allEditing();
+        model.addAttribute(articleList);
         return "articles";
     }
 }
